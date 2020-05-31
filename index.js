1125a774def8d14c734ec5722f3565b99561f253
harmonious-cache-6099 <isaac_scothern@hotmail.co.uk>
2020-05-31 13:55:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function getFirstSelector(selector){
   return document.querySelector('selector').innerHTML;
 }
+
+
+function nestedTarget(){
+  return document.querySelector('nested')getElementById(.target);
+}
