2da850731e1a74a9daf9f47467ccb87aa6111882
harmonious-cache-6099 <isaac_scothern@hotmail.co.uk>
2020-05-31 13:55:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function getFirstSelector(selector){
 
 
 function nestedTarget(){
-  return document.querySelector('nested')getElementById(.target);
+  return document.querySelector('nested')getElementById(target);
 }
